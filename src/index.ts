5ab81d8352e481d67823d4b6ab017652f9feb9a3
luoyjx <luoyjx@gmail.com>
2019-10-29 17:23:24 +0800

    Revert "perf: 传入的参数是 string 时，不再进行 stringify 操作"
    This reverts commit 3cdb8d7fcf4c84d006362bb9137894004e02db90.

--- src/index.ts
+++ src/index.ts
@@ -94,8 +94,7 @@ export async function createFunction(params: LambdaParams): Promise<Lambda> {
 	): Promise<T> {
 		const result = await fn.invoke({
 			InvocationType: 'RequestResponse',
-			Payload:
-				typeof payload === 'string' ? payload : JSON.stringify(payload)
+			Payload: JSON.stringify(payload)
 		});
 		let resultPayload = result.Payload;
 		if (typeof resultPayload !== 'string') {
